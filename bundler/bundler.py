08427f53d362d773dc7f03f7418b8bbbd887a37b
Andrew Wahid <andrew.wahid1@gmail.com>
2022-11-03 21:53:26 +0200

    Support multiple operations for eth_getRequestIds

--- bundler/bundler.py
+++ bundler/bundler.py
@@ -223,9 +223,9 @@ def eth_sendUserOperation(request) -> Result:
         return Error(2, "Bundle operation failed", data=str(inst))
 
 @method
-def eth_getRequestId(request) -> Result:
+def eth_getRequestIds(request) -> Result:
 
-    serialzer = OperationSerialzer(data=request)
+    serialzer = OperationSerialzer(data=request, many=True)
     
     if serialzer.is_valid():
         serialzer.save()
@@ -237,11 +237,15 @@ def eth_getRequestId(request) -> Result:
     abi = [{"inputs":[{"components":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"uint256","name":"nonce","type":"uint256"},{"internalType":"bytes","name":"initCode","type":"bytes"},{"internalType":"bytes","name":"callData","type":"bytes"},{"internalType":"uint256","name":"callGas","type":"uint256"},{"internalType":"uint256","name":"verificationGas","type":"uint256"},{"internalType":"uint256","name":"preVerificationGas","type":"uint256"},{"internalType":"uint256","name":"maxFeePerGas","type":"uint256"},{"internalType":"uint256","name":"maxPriorityFeePerGas","type":"uint256"},{"internalType":"address","name":"paymaster","type":"address"},{"internalType":"bytes","name":"paymasterData","type":"bytes"},{"internalType":"bytes","name":"signature","type":"bytes"}],"internalType":"struct UserOperation","name":"userOp","type":"tuple"}],"name":"getRequestId","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes","name":"initCode","type":"bytes"},{"internalType":"uint256","name":"salt","type":"uint256"}],"name":"getSenderAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]
     
     entryPoint = w3.eth.contract(address=env('entryPoint_add'), abi=abi)
-    opDict = serialzer.data
-   
-    requestId = entryPoint.functions.getRequestId(opDict).call()
 
-    return Success(requestId.hex())
+    ops = serialzer.data
+    result = []
+    for operation in ops:
+        opDict = dict(operation)
+        requestId = entryPoint.functions.getRequestId(opDict).call()
+        result.append(requestId.hex())
+
+    return Success(result)
 
 
 @method
