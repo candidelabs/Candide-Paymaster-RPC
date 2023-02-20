51fe9f62082220f503f43578ff8ee76c54d18dbd
Andrew Wahid <andrew.wahid1@gmail.com>
2023-02-20 04:52:55 +0200

    refactor: add django secret key to env

--- bundler/settings.py
+++ bundler/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-b6z_(y0=-lfcej62_u&bnzm$z+opp7kp=t%81)k9--2kb=r#gb"
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
 
