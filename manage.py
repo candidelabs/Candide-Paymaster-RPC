8e5f6faa11defaf9dbedad01316427faa600a983
Andrew Wahid <andrew.wahid1@gmail.com>
2023-04-06 21:37:30 +0200

    feat: add paymaster address to cli agrs

--- manage.py
+++ manage.py
@@ -10,6 +10,7 @@ def main():
     parser = argparse.ArgumentParser(description='Overwrite values in os.environ with optional command-line arguments')
     parser.add_argument('--port', help='port number', type=int)
     parser.add_argument('--chainId', help='The Ethereum Network chain ID', type=int)
+    parser.add_argument('--paymaster', help='The paymaster address', type=int)
     parser.add_argument('--HTTPProvider', help='The Ethereum Network URL HTTP provider', type=str)
     args, remaining_argv = parser.parse_known_args()
 
@@ -17,6 +18,8 @@ def main():
         os.environ["chainId"] = str(args.chainId)
     if args.HTTPProvider:
         os.environ["HTTPProvider"] = args.HTTPProvider
+    if args.paymaster:
+        os.environ["paymaster_add"] = args.paymaster
     if args.port:
         os.environ["PORT"] = str(args.port)
     
